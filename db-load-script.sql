0f64165ef436ba04d934f8ff7411dd61e680d4b1
System Administrator <root@vijays-MacBook-Pro.local>
2020-08-01 08:12:32 +0900

    Initial Database commit

---
+++ db-load-script.sql
@@ -0,0 +1,3 @@
+USE ecomdb;
+CREATE TABLE products (id mediumint(8) unsigned NOT NULL auto_increment,Name varchar(255) default NULL,Price varchar(255) default NULL, ImageUrl varchar(255) default NULL,PRIMARY KEY (id)) AUTO_INCREMENT=1;
+
